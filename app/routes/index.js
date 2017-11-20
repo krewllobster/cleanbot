de2c0c1b11ea596c70b22d6f45ee0307e07cf5f6
Sebastian <krewllobster@gmail.com>
2017-11-20 12:12:36 -0500

    touch ups to remove PNG conversion and saving and routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,37 +1,32 @@
-const path = require('path')
+const path = require('path');
 
 const {
   authController,
   commandController,
   actionController,
-  leaderBoardController,
-} = require('../controllers')
+  leaderBoardController
+} = require('../controllers');
 
-module.exports = (app) => {
-
+module.exports = app => {
   //public page
   app.get('/', (req, res) => {
-    console.log('rendering root')
-    res.render('root')
-  })
-
-  app.get('/leaderboards/images/*.png', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../leaderboards', req.params['0'] + '.png'))
-  })
+    console.log('rendering root');
+    res.render('root');
+  });
 
   app.get('/leaderboards/*', (req, res) => {
-    leaderBoardController(req, res)
-  })
+    leaderBoardController(req, res);
+  });
 
   app.get('/auth', (req, res) => {
-    authController(req, res)
-  })
+    authController(req, res);
+  });
 
   app.post('/slack/actions', (req, res) => {
-    actionController(req, res)
-  })
+    actionController(req, res);
+  });
 
   app.post('/slack/command', (req, res) => {
-    commandController(req, res)
-  })
-}
+    commandController(req, res);
+  });
+};
