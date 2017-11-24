6fed06ac4ecfa8c36009dc2b867f6c6b3a19fdbf
Sebastian <krewllobster@gmail.com>
2017-11-25 10:05:21 +1300

    add no question text

--- app/actions/send_question_list.js
+++ app/actions/send_question_list.js
@@ -1,38 +1,39 @@
-const { selectQuestionButtons, genLeaderboard } = require('../attachments')
-const { findFullThrowdown } = require('../common')
-
+const { selectQuestionButtons, genLeaderboard } = require('../attachments');
+const { findFullThrowdown } = require('../common');
 
 module.exports = async (payload, action, deps) => {
   const {
-    user: {id: user_id},
-    team: {id: team_id},
+    user: { id: user_id },
+    team: { id: team_id },
     message_ts,
-    channel: {id: channel_id},
-    original_message,
-  } = payload
+    channel: { id: channel_id },
+    original_message
+  } = payload;
 
-  const {
-    throwdown_id,
-    round,
-  } = JSON.parse(action.value)
+  const { throwdown_id, round } = JSON.parse(action.value);
 
-  const { slack, dbInterface, commandFactory, exec, user } = deps
+  const { slack, dbInterface, commandFactory, exec, user } = deps;
 
   const questionButtonText = `
     Choose a difficulty below to get this round's questions. We'll start timing you when you select a difficulty, and stop the timer when you select an answer.
 After you give an answer, you'll see this message again with your remaining questions.\nGood luck!!
-  `
+  `;
+  const noMoreQuestionText = `Look's like you're out of questions for round ${round}!`;
 
-  const noMoreQuestionText = `Look's like you're out of questions for round ${round}!`
+  const questionsToAttach = await selectQuestionButtons(
+    throwdown_id,
+    round,
+    deps
+  );
+  const questionNumber = questionsToAttach.length;
 
-  const questionsToAttach = await selectQuestionButtons(throwdown_id, round, deps)
-  console.log('going to ask')
-  console.log(questionsToAttach)
-  const sendQuestions = commandFactory('slack').setOperation('ephemeralMessage')
-    .setChannel(channel_id).setUser(user_id)
+  const sendQuestions = commandFactory('slack')
+    .setOperation('ephemeralMessage')
+    .setChannel(channel_id)
+    .setUser(user_id)
     .setAttachments(questionsToAttach)
-    .setText(questionButtonText)
-    .save()
+    .setText(questionNumber > 0 ? questionButtonText : noMoreQuestionText)
+    .save();
 
-  exec.one(slack, sendQuestions)
-}
+  exec.one(slack, sendQuestions);
+};
