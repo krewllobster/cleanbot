236929dd33d0837eaf5a387f97d6faa89dbf39c3
Sebastian <krewllobster@gmail.com>
2017-12-14 15:16:49 -0500

    fixing #20 -- throwdown author not added to private channel

--- app/actions/create_throwdown.js
+++ app/actions/create_throwdown.js
@@ -1,36 +1,33 @@
-const agenda              = require('../../agenda')
-const { singleThrowdown } = require('../attachments')
+const agenda = require('../../agenda');
+const { singleThrowdown } = require('../attachments');
 const {
   processing: processingMessage,
   findFullThrowdown,
   findRandom
-}  = require('../common')
-
-
+} = require('../common');
 
 module.exports = async (payload, submission, deps) => {
-
   const {
-    team: {id: team_id},
-    user: {id: user_id},
-    channel: {id: channel_id},
-  } = payload
+    team: { id: team_id },
+    user: { id: user_id },
+    channel: { id: channel_id }
+  } = payload;
 
-  const {name, description, category, privacy, start_date} = submission
-  const {slack, dbInterface, commandFactory, exec, user} = deps
+  const { name, description, category, privacy, start_date } = submission;
+  const { slack, dbInterface, commandFactory, exec, user } = deps;
 
   const processing = processingMessage(deps, {
     text: 'Processing new Throwdown...',
-    user_id,
-  })
+    user_id
+  });
 
-  const {channel} = await processing.next().value
-  const {ts} = await processing.next(channel).value
+  const { channel } = await processing.next().value;
+  const { ts } = await processing.next(channel).value;
 
-  const errorHandle = (err) => {
-    console.log(err)
-    throw new Error('error creating throwdown::' + err)
-  }
+  const errorHandle = err => {
+    console.log(err);
+    throw new Error('error creating throwdown::' + err);
+  };
 
   const newThrowdown = {
     privacy,
@@ -42,194 +39,260 @@ module.exports = async (payload, submission, deps) => {
     participants: [user._id],
     categories: [category],
     invitees: []
-  }
+  };
 
   //check name and send error message if problem found
-  const errorBase = commandFactory('slack').setOperation('updateMessage')
-    .setChannel(channel.id).setTs(ts)
+  const errorBase = commandFactory('slack')
+    .setOperation('updateMessage')
+    .setChannel(channel.id)
+    .setTs(ts);
 
-  if(name) {
-    const getGroupList = commandFactory('slack').setClient('userClient')
-      .setOperation('getChannels').save()
+  if (name) {
+    const getGroupList = commandFactory('slack')
+      .setClient('userClient')
+      .setOperation('getChannels')
+      .save();
 
-    const channelName = name.split(' ').join('_').toLowerCase()
-
-    if(channelName.match(/[^(a-z|0-9|_|\-)]/) || channelName.length > 21) {
-      console.log('throwdown title had invalid character or was too long')
+    const channelName = name
+      .split(' ')
+      .join('_')
+      .toLowerCase();
+    //if name has capitals, numbers, underscores or hyphens, or is longer than 21, send error
+    if (channelName.match(/[^(a-z|0-9|_|\-)]/) || channelName.length > 21) {
+      console.log('throwdown title had invalid character or was too long');
       const invalidNameMessage = errorBase
-        .setText('Invalid Throwdown Name. Please try again').save()
+        .setText('Invalid Throwdown Name. Please try again')
+        .save();
 
-      return await exec.one(slack, invalidNameMessage)
+      return await exec.one(slack, invalidNameMessage);
     }
 
-    const {groups} = await exec.one(slack, getGroupList)
+    const { groups } = await exec.one(slack, getGroupList);
 
-    if(groups && groups.some(c => c.name === channelName)) {
-      console.log('throwdown channel name was already taken')
+    //if public, private, or archived channel shares name, send error
+    if (groups && groups.some(c => c.name === channelName)) {
+      console.log('throwdown channel name was already taken');
       const nameTakenMessage = errorBase
-        .setText('That name is already taken. Please try again!').save()
+        .setText('That name is already taken. Please try again!')
+        .save();
 
-      return await exec.one(slack, nameTakenMessage)
+      return await exec.one(slack, nameTakenMessage);
     }
   }
 
-  console.log('creating new throwdown in database')
-  const findOrCreateThrowdown =
-    commandFactory('db').setEntity('Throwdown').setOperation('findOrCreate')
-    .setMatch({name}).setUpdate(newThrowdown).save()
+  console.log('creating new throwdown in database');
 
-  const {doc, created} =
-    await exec.one(dbInterface, findOrCreateThrowdown)
+  const findOrCreateThrowdown = commandFactory('db')
+    .setEntity('Throwdown')
+    .setOperation('findOrCreate')
+    .setMatch({ name })
+    .setUpdate(newThrowdown)
+    .save();
 
-  const newThrowdownMessage =
-    commandFactory('slack').setOperation('updateMessage')
-      .setChannel(channel.id).setTs(ts)
+  const { doc, created } = await exec.one(dbInterface, findOrCreateThrowdown);
 
-  let responseMessage, throwdown
+  const newThrowdownMessage = commandFactory('slack')
+    .setOperation('updateMessage')
+    .setChannel(channel.id)
+    .setTs(ts);
 
-  if(!created) {
-    console.log('throwdown creation was unsuccessful')
-    responseMessage =
-      newThrowdownMessage.setText(`There was an error creating your Throwdown. Please try again`)
-      .setAttachments([]).save()
+  let responseMessage, throwdown;
 
-    return await exec.one(slack, responseMessage)
+  if (!created) {
+    console.log('throwdown creation was unsuccessful');
+    responseMessage = newThrowdownMessage
+      .setText(`There was an error creating your Throwdown. Please try again`)
+      .setAttachments([])
+      .save();
+
+    return await exec.one(slack, responseMessage);
   }
 
-  console.log('throwdown successfully created')
-  throwdown = await findFullThrowdown(deps, {matchFields: {_id: doc._id}})
+  console.log('throwdown successfully created');
 
-  responseMessage = newThrowdownMessage.setText(
-    `Congratulations, your Throwdown has been set up! \n ${
-      throwdown.privacy === 'private'
+  throwdown = await findFullThrowdown(deps, { matchFields: { _id: doc._id } });
+
+  responseMessage = newThrowdownMessage
+    .setText(
+      `Congratulations, your Throwdown has been set up! \n ${throwdown.privacy ===
+      'private'
         ? 'You can invite people using the invite button below.'
-        : 'Your throwdown will now show up for anyone to join.'
-    }`)
-    .setAttachments([
-      singleThrowdown(throwdown, user_id, false)
-    ]).save()
+        : 'Your throwdown will now show up for anyone to join.'}`
+    )
+    .setAttachments([singleThrowdown(throwdown, user_id, false)])
+    .save();
 
-  const sendResponse = await exec.one(slack, responseMessage)
+  const sendResponse = await exec.one(slack, responseMessage);
 
   const [updatedThrowdown, result] = await Promise.all([
-    initQuestions(throwdown, deps), initChannel(throwdown, deps)
-  ])
+    initQuestions(throwdown, deps),
+    initChannel(throwdown, deps)
+  ]);
+  //CHANGE QUESTION TIMING HERE
+  console.log('initiating recurring throwdown jobs');
+  const questionsJob = agenda.create('send question buttons', {
+    throwdown_id: updatedThrowdown._id,
+    team_id: team_id,
+    user
+  });
 
-  console.log('initiating recurring throwdown jobs')
-  const questionsJob = agenda
-    .create('send question buttons', {
-      throwdown_id: updatedThrowdown._id,
-      team_id: team_id,
-      user,
-    })
-
-  questionsJob.repeatEvery('12 hours')
-  questionsJob.save()
-}
+  questionsJob.repeatEvery('12 hours');
+  questionsJob.save();
+};
 
 const initChannel = async (throwdown, deps) => {
-  console.log('initializing throwdown channel')
+  console.log('initializing throwdown channel');
   return new Promise(async (resolve, reject) => {
-    const {slack, dbInterface, commandFactory, exec, user} = deps
-    const name = throwdown.name.split(' ').join('_').toLowerCase()
+    const { slack, dbInterface, commandFactory, exec, user } = deps;
 
-    const createChannel = commandFactory('slack').setOperation('createConversation')
-      .set({name, private: true}).setClient('userClient').save()
+    const name = throwdown.name
+      .split(' ')
+      .join('_')
+      .toLowerCase();
 
-    const {channel} = await exec.one(slack, createChannel)
+    const createChannel = commandFactory('slack')
+      .setOperation('createConversation')
+      .set({ name, private: true })
+      .setClient('userClient')
+      .save();
 
-    const setThrowdownChannel = commandFactory('db').setEntity('Throwdown')
-      .setOperation('findOneAndUpdate').setMatch({_id: throwdown._id})
-      .setUpdate({channel: channel.id}).save()
+    const { creator, channel } = await exec.one(slack, createChannel);
 
-    const setChannelTopic = commandFactory('slack').setOperation('setTopic')
-      .setChannel(channel.id).setTopic(throwdown.description)
-      .setClient('userClient').save()
+    const setThrowdownChannel = commandFactory('db')
+      .setEntity('Throwdown')
+      .setOperation('findOneAndUpdate')
+      .setMatch({ _id: throwdown._id })
+      .setUpdate({ channel: channel.id })
+      .save();
 
-    const setChannelPurpose = commandFactory('slack').setOperation('setPurpose')
-      .setChannel(channel.id).setPurpose(`<@${throwdown.created_by.user_id}>'s challenge`)
-      .setClient('userClient').save()
+    const setChannelTopic = commandFactory('slack')
+      .setOperation('setTopic')
+      .setChannel(channel.id)
+      .setTopic(throwdown.description)
+      .setClient('userClient')
+      .save();
+
+    const setChannelPurpose = commandFactory('slack')
+      .setOperation('setPurpose')
+      .setChannel(channel.id)
+      .setPurpose(`<@${throwdown.created_by.user_id}>'s challenge`)
+      .setClient('userClient')
+      .save();
 
     const responses = await exec.many([
-      [dbInterface, setThrowdownChannel], [slack, setChannelTopic],
+      [dbInterface, setThrowdownChannel],
+      [slack, setChannelTopic],
       [slack, setChannelPurpose]
-    ])
+    ]);
 
-    const getBotId = commandFactory('db').setEntity('Team').setOperation('findOne')
-      .setMatch({team_id: throwdown.team_id}).save()
+    const getBotId = commandFactory('db')
+      .setEntity('Team')
+      .setOperation('findOne')
+      .setMatch({ team_id: throwdown.team_id })
+      .save();
 
-    const {bot: {bot_user_id}} = await exec.one(dbInterface, getBotId)
+    const { bot: { bot_user_id } } = await exec.one(dbInterface, getBotId);
 
-    let usersToInvite = bot_user_id
+    let usersToInvite = bot_user_id;
 
-    if(throwdown.created_by.user_id !== user.user_id) {
-      usersToInvite += `,${throwdown.created_by.user_id}`
+    if (throwdown.created_by.user_id !== creator) {
+      usersToInvite += `,${throwdown.created_by.user_id}`;
     }
 
-    const inviteBot = commandFactory('slack').setOperation('inviteToConversation')
-      .setClient('userClient').setChannel(channel.id)
-      .setUsers(usersToInvite).save()
+    const inviteBot = commandFactory('slack')
+      .setOperation('inviteToConversation')
+      .setClient('userClient')
+      .setChannel(channel.id)
+      .setUsers(usersToInvite)
+      .save();
 
-    const response = await exec.one(slack, inviteBot)
+    const response = await exec.one(slack, inviteBot);
 
-    resolve(response)
-  })
-}
+    resolve(response);
+  });
+};
 
 const initQuestions = (throwdown, deps) => {
-  console.log('initializing throwdown questions')
-  const errorHandle = (err) => {
-    console.log(err)
-    throw new Error('error creating throwdown::' + err)
-  }
+  console.log('initializing throwdown questions');
+  const errorHandle = err => {
+    console.log(err);
+    throw new Error('error creating throwdown::' + err);
+  };
   return new Promise(async (resolve, reject) => {
-    const {slack, dbInterface, commandFactory, exec, user} = deps
-    const categories = throwdown.categories.map(c => c._id)
+    const { slack, dbInterface, commandFactory, exec, user } = deps;
+    const categories = throwdown.categories.map(c => c._id);
 
-    const getQuestions = commandFactory('db').setEntity('Question')
-      .setOperation('find').setMatch({category: {$in: categories}}).save()
+    const getQuestions = commandFactory('db')
+      .setEntity('Question')
+      .setOperation('find')
+      .setMatch({ category: { $in: categories } })
+      .save();
 
-    const allQuestions = await exec.one(dbInterface, getQuestions)
+    const allQuestions = await exec.one(dbInterface, getQuestions);
 
-    let easy = findRandom(allQuestions.filter(q => q.difficulty === 'easy'), 10)
-    let medium = findRandom(allQuestions.filter(q => q.difficulty === 'medium'), 10)
-    let hard = findRandom(allQuestions.filter(q => q.difficulty === 'hard'), 10)
+    let easy = findRandom(
+      allQuestions.filter(q => q.difficulty === 'easy'),
+      10
+    );
+    let medium = findRandom(
+      allQuestions.filter(q => q.difficulty === 'medium'),
+      10
+    );
+    let hard = findRandom(
+      allQuestions.filter(q => q.difficulty === 'hard'),
+      10
+    );
 
-    const total = easy.length + medium.length + hard.length
-    console.log(total)
-    if(total < 30) {
-      console.log('fewer than 30 questions returned, fetching general questions')
-      const getGeneral = commandFactory('db').setEntity('Question')
-        .setOperation('find').setMatch({category: 9}).save()
+    const total = easy.length + medium.length + hard.length;
+    console.log(total);
+    if (total < 30) {
+      console.log(
+        'fewer than 30 questions returned, fetching general questions'
+      );
+      const getGeneral = commandFactory('db')
+        .setEntity('Question')
+        .setOperation('find')
+        .setMatch({ category: 9 })
+        .save();
 
-      const extras = await exec.one(dbInterface, getGeneral)
+      const extras = await exec.one(dbInterface, getGeneral);
 
-      const easyExtra =
-        findRandom(extras.filter(q => q.difficulty === 'easy'), 10-easy.length)
-      const mediumExtra =
-        findRandom(extras.filter(q => q.difficulty === 'medium'), 10-medium.length)
-      const hardExtra =
-        findRandom(extras.filter(q => q.difficulty === 'hard'), 10-hard.length)
+      const easyExtra = findRandom(
+        extras.filter(q => q.difficulty === 'easy'),
+        10 - easy.length
+      );
+      const mediumExtra = findRandom(
+        extras.filter(q => q.difficulty === 'medium'),
+        10 - medium.length
+      );
+      const hardExtra = findRandom(
+        extras.filter(q => q.difficulty === 'hard'),
+        10 - hard.length
+      );
 
-      easy = [...easy, ...easyExtra]
-      medium = [...medium, ...mediumExtra]
-      hard = [...hard, ...hardExtra]
+      easy = [...easy, ...easyExtra];
+      medium = [...medium, ...mediumExtra];
+      hard = [...hard, ...hardExtra];
     }
     const filter = (q, i) => {
-      return ({question: q._id, round: i + 1})
-    }
-    easyQuestions = easy.map(filter)
-    medQuestions = medium.map(filter)
-    hardQuestions = hard.map(filter)
+      return { question: q._id, round: i + 1 };
+    };
+    easyQuestions = easy.map(filter);
+    medQuestions = medium.map(filter);
+    hardQuestions = hard.map(filter);
 
-    const questions = [...easyQuestions, ...medQuestions, ...hardQuestions]
+    const questions = [...easyQuestions, ...medQuestions, ...hardQuestions];
 
-    const updateThrowdown = commandFactory('db').setEntity('Throwdown')
-      .setOperation('findOneAndUpdate').setMatch({_id: throwdown._id})
-      .setUpdate({questions}).setOptions({new: true}).save()
+    const updateThrowdown = commandFactory('db')
+      .setEntity('Throwdown')
+      .setOperation('findOneAndUpdate')
+      .setMatch({ _id: throwdown._id })
+      .setUpdate({ questions })
+      .setOptions({ new: true })
+      .save();
 
-    const updated = await exec.one(dbInterface, updateThrowdown)
+    const updated = await exec.one(dbInterface, updateThrowdown);
 
-    resolve(updated)
-  })
-}
+    resolve(updated);
+  });
+};
