5261c20cc77a00d1c0355054bc355e6ebd83d111
Sebastian <krewllobster@gmail.com>
2017-12-22 18:43:54 -0500

    jobs for throwdowns over round 10 now self-delete

--- app/jobs/sendQuestions.js
+++ app/jobs/sendQuestions.js
@@ -34,6 +34,12 @@ module.exports = function(agenda) {
 
     if (fullThrowdown.round > 10) {
       console.log('throwdown round is past 10, need to terminate job');
+      agenda.cancel(
+        { attrs: { data: { round: { gt: 10 } } } },
+        (err, numRemove) => {
+          console.log(`successfully removed ${numRemove} job(s)`);
+        }
+      );
       return done();
     }
 
