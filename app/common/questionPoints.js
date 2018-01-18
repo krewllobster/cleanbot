dc30f1f785f2baada27ee4108e81465849a1f667
Sebastian <krewllobster@gmail.com>
2018-01-18 13:14:34 -0500

    points now weighted for difficulty

--- app/common/questionPoints.js
+++ app/common/questionPoints.js
@@ -1,4 +1,5 @@
 module.exports = ({ correct, duration, bonus, difficulty }) => {
+  console.log('question points diff: ', difficulty);
   const diffPoints = {
     easy: 30,
     medium: 50,
@@ -6,10 +7,19 @@ module.exports = ({ correct, duration, bonus, difficulty }) => {
     unknown: 50
   };
 
+  const diffDurations = {
+    easy: 3,
+    medium: 2,
+    hard: 1,
+    unknown: 2
+  };
+
   if (bonus && correct) return 20;
   if (bonus && !correct) return 0;
 
-  let points = (correct ? 50 : 0) - duration * 3;
+  let points =
+    (correct ? diffPoints[difficulty] : 0) -
+    duration * diffDurations[difficulty];
   if (correct && points <= 10) points = 10;
   if (!correct && points <= -20) points = -20;
   return correct ? Math.ceil(points) : Math.floor(points);
